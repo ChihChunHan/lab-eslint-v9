eebad05325555c81c223a5e0feb43538c8b3bef2
hanc.han <chihchun.han@gmail.com>
2024-05-28 14:45:49 +0800

    init vite && eslint-plugin-vue

--- app.js
+++
@@ -1,7 +0,0 @@
-const a = "";
-
-const foo = t => {
-  return t;
-};
-
-foo(a);
--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import js from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   // https://eslint.org/docs/latest/use/configure/configuration-files#using-predefined-configurations
   js.configs.recommended,
+  // https://eslint.vuejs.org/user-guide/
+  ...pluginVue.configs["flat/recommended"],
   // https://github.com/prettier/eslint-plugin-prettier#configuration-new-eslintconfigjs
   eslintPluginPrettierRecommended
 ];
